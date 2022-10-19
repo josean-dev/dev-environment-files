27bcf7459a60f12962bc98dcc955314befb98ab3
Josean Martinez <joseanmartinez@macbook-pro.local>
2022-10-19 11:49:03 -0400

    Added keymaps for special typescript functionality to remove unused variables and organize imports

--- .config/nvim/lua/josean/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/josean/plugins/lsp/lspconfig.lua
@@ -39,7 +39,9 @@ local on_attach = function(client, bufnr)
 
 	-- typescript specific keymaps (e.g. rename file and update imports)
 	if client.name == "tsserver" then
-		keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>")
+		keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
+		keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
+		keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
 	end
 end
 
