982da181d62e1d12634be0158b6bd3eaeb78b87f
Youtube <youtube@macbook-pro.local>
2022-10-20 16:54:45 -0400

    Added after option to nvim-ts-autotag to fix error

--- .config/nvim/lua/josean/plugins-setup.lua
+++ .config/nvim/lua/josean/plugins-setup.lua
@@ -94,7 +94,7 @@ return packer.startup(function(use)
 
 	-- auto closing
 	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-	use("windwp/nvim-ts-autotag") -- autoclose tags
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
