ceb94f555d3f79b3203864642f2b7c5405f685ed
Josean Martinez <joseanmartinez@macbook-pro.local>
2022-10-23 01:20:11 -0400

    Added condition to null-ls eslint to enable if eslint config file is present

--- .config/nvim/lua/josean/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/josean/plugins/lsp/null-ls.lua
@@ -19,7 +19,12 @@ null_ls.setup({
 		--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
 		formatting.prettier, -- js/ts formatter
 		formatting.stylua, -- lua formatter
-		diagnostics.eslint_d, -- js/ts linter
+		diagnostics.eslint_d.with({ -- js/ts linter
+			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
+			condition = function(utils)
+				return utils.root_has_file(".eslintrc.js")
+			end,
+		}),
 	},
 	-- configure format on save
 	on_attach = function(current_client, bufnr)
