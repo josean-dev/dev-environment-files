61c54117e5116e84129b54029dd7eb957177a95a
josean-dev <youtube@josean.com>
2022-10-25 16:40:06 -0400

    Removed supertab functionality for nvim-cmp as it was causing issues for me

--- .config/nvim/lua/josean/plugins/nvim-cmp.lua
+++ .config/nvim/lua/josean/plugins/nvim-cmp.lua
@@ -21,12 +21,6 @@ require("luasnip/loaders/from_vscode").lazy_load()
 
 vim.opt.completeopt = "menu,menuone,noselect"
 
--- helper function for super tab functionality (not in youtube nvim video)
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -41,29 +35,6 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 		["<C-e>"] = cmp.mapping.abort(), -- close completion window
 		["<CR>"] = cmp.mapping.confirm({ select = false }),
-
-		-- super tab functionality (not in youtube nvim video)
-		["<Tab>"] = cmp.mapping(function(fallback) -- use tab for next suggestion
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-
-		["<S-Tab>"] = cmp.mapping(function(fallback) -- use shift-tab for prev suggestion
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
 	}),
 	-- sources for autocompletion
 	sources = cmp.config.sources({
