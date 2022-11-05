37e5fe664282f2e87dc1b4d3df9d8c4e8ef73b5c
josean-dev <youtube@josean.com>
2022-11-05 17:27:41 -0400

    Changed keybinds for moving up and down with nvim-cmp, telescope and lspsaga to <C-p> and <C-n>

--- .config/nvim/lua/josean/plugins/telescope.lua
+++ .config/nvim/lua/josean/plugins/telescope.lua
@@ -16,8 +16,8 @@ telescope.setup({
   defaults = {
     mappings = {
       i = {
-        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-        ["<C-j>"] = actions.move_selection_next, -- move to next result
+        ["<C-p>"] = actions.move_selection_previous, -- move to prev result
+        ["<C-n>"] = actions.move_selection_next, -- move to next result
         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
       },
     },
