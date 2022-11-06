1c8890fd956e73f5d8bb0246c27c045962e50a73
josean-dev <youtube@josean.com>
2022-11-06 17:35:01 -0400

    Added emmet_ls lsp to the config

--- .config/nvim/lua/josean/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/josean/plugins/lsp/lspconfig.lua
@@ -82,6 +82,13 @@ lspconfig["tailwindcss"].setup({
   on_attach = on_attach,
 })
 
+-- configure emmet language server
+lspconfig["emmet_ls"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+})
+
 -- configure lua server (with special settings)
 lspconfig["sumneko_lua"].setup({
   capabilities = capabilities,
--- .config/nvim/lua/josean/plugins/lsp/mason.lua
+++ .config/nvim/lua/josean/plugins/lsp/mason.lua
@@ -27,6 +27,7 @@ mason_lspconfig.setup({
     "cssls",
     "tailwindcss",
     "sumneko_lua",
+    "emmet_ls",
   },
   -- auto-install configured servers (with lspconfig)
   automatic_installation = true, -- not the same as ensure_installed
