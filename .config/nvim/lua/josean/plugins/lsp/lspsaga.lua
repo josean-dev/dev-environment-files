7be90c7152a51a11cc58fe7ce3b1f8b42c19a8fc
josean-dev <youtube@josean.com>
2023-01-15 09:28:40 -0400

    Migrated lspsaga config to new version

--- .config/nvim/lua/josean/plugins/lsp/lspsaga.lua
+++ .config/nvim/lua/josean/plugins/lsp/lspsaga.lua
@@ -4,15 +4,11 @@ if not saga_status then
   return
 end
 
-saga.init_lsp_saga({
+saga.setup({
   -- keybinds for navigation in lspsaga window
-  move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-  -- use enter to open file with finder
-  finder_action_keys = {
-    open = "<CR>",
-  },
+  scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
   -- use enter to open file with definition preview
-  definition_action_keys = {
+  definition = {
     edit = "<CR>",
   },
 })
