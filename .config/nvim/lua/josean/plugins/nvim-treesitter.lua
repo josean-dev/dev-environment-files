29dc9b11ca90ed1733b2aae980df1f277a3bd591
josean-dev <youtube@josean.com>
2023-10-04 16:36:44 -0400

    Added advanced treesitter and treesitter text objects functionality

--- .config/nvim/lua/josean/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/josean/plugins/nvim-treesitter.lua
@@ -4,6 +4,7 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
     dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
       "windwp/nvim-ts-autotag",
     },
     config = function()
@@ -41,13 +42,20 @@ return {
           "dockerfile",
           "gitignore",
         },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
         -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
         context_commentstring = {
           enable = true,
           enable_autocmd = false,
         },
-        -- auto install above language parsers
-        auto_install = true,
       })
     end,
   },
