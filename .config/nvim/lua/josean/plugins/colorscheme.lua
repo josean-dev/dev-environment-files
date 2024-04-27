774e61dd38e7e11592abcf2a4f525020fa9bce51
josean-dev <youtube@josean.com>
2024-04-27 19:14:04 -0400

    Added support for transparent colorschme

--- .config/nvim/lua/josean/plugins/colorscheme.lua
+++ .config/nvim/lua/josean/plugins/colorscheme.lua
@@ -2,6 +2,8 @@ return {
   "folke/tokyonight.nvim",
   priority = 1000,
   config = function()
+    local transparent = false -- set to true if you would like to enable transparency
+
     local bg = "#011628"
     local bg_dark = "#011423"
     local bg_highlight = "#143652"
@@ -14,15 +16,20 @@ return {
 
     require("tokyonight").setup({
       style = "night",
+      transparent = transparent,
+      styles = {
+        sidebars = transparent and "transparent" or "dark",
+        floats = transparent and "transparent" or "dark",
+      },
       on_colors = function(colors)
         colors.bg = bg
-        colors.bg_dark = bg_dark
-        colors.bg_float = bg_dark
+        colors.bg_dark = transparent and colors.none or bg_dark
+        colors.bg_float = transparent and colors.none or bg_dark
         colors.bg_highlight = bg_highlight
         colors.bg_popup = bg_dark
         colors.bg_search = bg_search
-        colors.bg_sidebar = bg_dark
-        colors.bg_statusline = bg_dark
+        colors.bg_sidebar = transparent and colors.none or bg_dark
+        colors.bg_statusline = transparent and colors.none or bg_dark
         colors.bg_visual = bg_visual
         colors.border = border
         colors.fg = fg
@@ -30,9 +37,9 @@ return {
         colors.fg_float = fg
         colors.fg_gutter = fg_gutter
         colors.fg_sidebar = fg_dark
-      end
+      end,
     })
 
     vim.cmd("colorscheme tokyonight")
-  end
+  end,
 }
