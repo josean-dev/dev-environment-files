443d81eff2d0e1fa029896d352dc26f1ebd20e8c
MrCee <10969457+MrCee@users.noreply.github.com>
2024-05-31 09:11:41 +0000

    telescope fix and nvim-tree updated

--- .config/nvim/lua/josean/plugins/nvim-tree.lua
+++ .config/nvim/lua/josean/plugins/nvim-tree.lua
@@ -39,9 +39,7 @@ return {
       },
       filters = {
         custom = { ".DS_Store" },
-      },
-      git = {
-        ignore = false,
+        git_ignored = false,
       },
     })
 
@@ -52,5 +50,5 @@ return {
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
 }
--- .config/nvim/lua/josean/plugins/telescope.lua
+++ .config/nvim/lua/josean/plugins/telescope.lua
@@ -10,27 +10,18 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
-    local transform_mod = require("telescope.actions.mt").transform_mod
 
-    local trouble = require("trouble")
-    local trouble_telescope = require("trouble.providers.telescope")
-
-    -- or create your custom action
-    local custom_actions = transform_mod({
-      open_trouble_qflist = function(prompt_bufnr)
-        trouble.toggle("quickfix")
-      end,
-    })
+    -- Update the import for trouble sources
+    local trouble = require("trouble.sources.telescope")
 
     telescope.setup({
       defaults = {
         path_display = { "smart" },
         mappings = {
           i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-q>"] = actions.send_selected_to_qflist,
           },
         },
       },
@@ -38,8 +29,7 @@ return {
 
     telescope.load_extension("fzf")
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+    local keymap = vim.keymap
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
