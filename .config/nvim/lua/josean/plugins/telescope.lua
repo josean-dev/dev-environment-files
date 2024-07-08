6614ff76b7975689853f45e6b520c7e8ece9984a
josean-dev <youtube@josean.com>
2024-07-08 11:39:15 -0400

    Updated trouble.nvim setup to v3

--- .config/nvim/lua/josean/plugins/telescope.lua
+++ .config/nvim/lua/josean/plugins/telescope.lua
@@ -13,7 +13,7 @@ return {
     local transform_mod = require("telescope.actions.mt").transform_mod
 
     local trouble = require("trouble")
-    local trouble_telescope = require("trouble.providers.telescope")
+    local trouble_telescope = require("trouble.sources.telescope")
 
     -- or create your custom action
     local custom_actions = transform_mod({
@@ -30,7 +30,7 @@ return {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+            ["<C-t>"] = trouble_telescope.open,
           },
         },
       },
--- .config/nvim/lua/josean/plugins/trouble.lua
+++ .config/nvim/lua/josean/plugins/trouble.lua
@@ -1,12 +1,15 @@
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+  opts = {
+    focus = true,
+  },
+  cmd = "Trouble",
   keys = {
-    { "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
-    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-    { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-    { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+    { "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
+    { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Open trouble document diagnostics" },
+    { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
+    { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
+    { "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
   },
 }
